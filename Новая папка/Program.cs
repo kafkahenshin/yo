dd1d54ee05e6d0907b041ef43da3486d23d4ec5e
kafkahenshin <gotokhel@gmail.com>
2021-09-03 18:58:24 +0300

    first

---
+++ Новая папка/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Students
+{
+
+    class Program
+    {
+        /// <summary>
+        /// This method adds record to the dictionary
+        /// and prints about it in console.
+        /// </summary>
+        public static void AddSubjects(Dictionary<Student, HashSet<string>> dictionary, Student student, string[] subjects)
+        {
+            dictionary[student] = new HashSet<string>(subjects);
+            Console.WriteLine("{0} -> {1}", string.Join(", ", subjects), student.ToString());
+        }
+
+        /// <summary>
+        /// This method generate new string[] of 3 elements
+        /// with distinct values.
+        /// </summary>
+        public static string[] Generate3Subjects(string[] subjects)
+        {
+            if (subjects == null)
+            {
+                throw new System.ArgumentNullException();
+            }
+
+            if (subjects.Length == 0)
+            {
+                throw new System.ArgumentException(message: "Array is empty");
+            }
+
+            string[] studentSubjects = new string[3];
+            var random = new Random();
+            studentSubjects[0] = subjects[random.Next(0, 5)];
+            for (var i = 1; studentSubjects[2] == null;)
+            {
+                var newSubjectIndex = random.Next(0, 5);
+                var duplicate = Array.Find(studentSubjects, subject => subject == subjects[newSubjectIndex]);
+                studentSubjects[i] = duplicate == null ? subjects[newSubjectIndex] : null;
+                if (studentSubjects[i] != null)
+                {
+                    i++;
+                }
+            }
+
+            return studentSubjects;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var student1c1 = new Student("John", "Smith");
+            var student2c1 = new Student("River", "Song");
+            var student3c1 = new Student("Amy", "Pond");
+            var student1c2 = new Student("John", "Smith");
+            var student2c2 = new Student("River", "Song");
+            var student3c2 = new Student("Amy", "Pond");
+            var subjects = new string[] { "math", "computer science", "biology", "history", "physics", "chemistry" };
+            var studentSubjectDict = new Dictionary<Student, HashSet<string>>();
+            AddSubjects(studentSubjectDict, student1c1, Generate3Subjects(subjects));
+            AddSubjects(studentSubjectDict, student2c1, Generate3Subjects(subjects));
+            AddSubjects(studentSubjectDict, student3c1, Generate3Subjects(subjects));
+            AddSubjects(studentSubjectDict, student1c2, Generate3Subjects(subjects));
+            AddSubjects(studentSubjectDict, student2c2, Generate3Subjects(subjects));
+            AddSubjects(studentSubjectDict, student3c2, Generate3Subjects(subjects));
+            Console.WriteLine("\n\nDictionary now is:\n");
+            foreach (var item in studentSubjectDict)
+            {
+                Console.WriteLine($"{string.Join(",", item.Value)} -- {item.Key.ToString()}");
+            }
+
+        }
+    }
+}
---
+++ Новая папка/Student.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Globalization;
+
+namespace Students
+{
+    /// <summary>
+    /// This class represents a student.
+    /// </summary>
+    internal sealed class Student
+    {
+        /// <summary>
+        /// Creates an instance based on name and surname.
+        /// </summary>
+        public Student(string name, string surname)
+        {
+            if (name == null || surname == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (name == "" || surname == "")
+            {
+                throw new ArgumentException("Name or surname is empty.");
+            }
+
+            if (!ValidateNameParameter(name) || !ValidateNameParameter(surname))
+            {
+                throw new ArgumentException("Name or surname consists of invalid characters.");
+            }
+
+            FullName = Capitalize(string.Format("{0} {1}", name.ToLower(), surname.ToLower()));
+            Email = string.Format("{0}.{1}@epam.com", name, surname).ToLower();
+        }
+
+        /// <summary>
+        /// Creates an instance based on email.
+        /// </summary>
+        public Student(string email)
+        {
+            if (email == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (email == "")
+            {
+                throw new ArgumentException("Email is empty.");
+            }
+
+            try
+            {
+                FullName = GetFullNameFromEmail(email.ToLower());
+            }
+
+            catch
+            {
+                throw;
+            }
+
+            Email = email.ToLower();
+        }
+
+        /// <summary>
+        /// Compares this instance to another object or Student instance.
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is Student student)
+            {
+                return student.FullName.Equals(FullName);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns hash code for this instance.
+        /// </summary>
+        public override int GetHashCode()
+        {
+            var positions = FullName.Length / 2;
+            return FullName.GetHashCode() >> positions
+                    ^ Email.GetHashCode() << positions;
+        }
+
+        /// <summary>
+        /// Returns this instance data in string format.
+        /// </summary>
+        public override string ToString()
+        {
+            return $"{FullName} : {Email}";
+        }
+
+        /// <summary>
+        /// Name and Surname in one string.
+        /// </summary>
+        public string FullName { get; }
+
+        /// <summary>
+        /// Email.
+        /// </summary>
+        public string Email { get; }
+
+        private string Capitalize(string uncapitalized)
+        {
+            var textInfo = new CultureInfo("en-US", false).TextInfo;
+            return textInfo.ToTitleCase(textInfo.ToLower(uncapitalized));
+        }
+
+        private bool ValidateNameParameter(string name)
+        {
+            var nameArray = name.ToCharArray();
+            var length = name.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (!char.IsLetter(nameArray[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private string GetFullNameFromEmail(string email)
+        {
+            var fullname = email.Replace("@epam.com", "");
+            var splittedFullname = fullname.Split('.');
+            if (splittedFullname.Length != 2
+                || !ValidateNameParameter(splittedFullname[0])
+                || !ValidateNameParameter(splittedFullname[1]))
+            {
+                throw new ArgumentException("Email is in wrong format");
+            }
+
+            return string.Concat(Capitalize(splittedFullname[0]), " ", Capitalize(splittedFullname[1]));
+        }
+    }
+}
